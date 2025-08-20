e8a4d77f341e6a417b870baab251f75daf112fc3
Pradeeptamilvel <tamilvelpradeep583@gmail.com>
2025-08-21 00:02:00 +0530

    created form components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormBuilderComponent } from './form-builder/form-builder.component';
+import { FormEditorComponent } from './form-editor/form-editor.component';
+import { FormControlComponent } from './form-control/form-control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormBuilderComponent,
+    FormEditorComponent,
+    FormControlComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/form-editor/form-editor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-editor',
+  standalone: false,
+  templateUrl: './form-editor.component.html',
+  styleUrl: './form-editor.component.scss'
+})
+export class FormEditorComponent {
+
+}
